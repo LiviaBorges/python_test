b1c623ae5933acf2ac2a9484f94e41c8a30c1577
Livia Borges <liviapessanha@outlook.com>
2025-05-03 00:30:20 +0200

    First commit

--- main.py
+++
@@ -1,9 +0,0 @@
-def in_autotests_we_trust(a, b):
-    if a == b:
-        print('Passou no teste')
-    else:
-        print('Falhou no teste')
-
-in_autotests_we_trust(10, '10')
-
-in_autotests_we_trust(0, False)
